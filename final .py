102593b33cd3b1998346f573073081e765eec719
rezajobs <120446719+rezajobs@users.noreply.github.com>
2024-06-13 18:57:13 -0400

    Create final .py

---
+++ final .py
@@ -0,0 +1,10 @@
+list_city = input('Write name of the city :').split()
+temperatures = list(map(int, input('Write a temperatures for each of them : ').split()))
+
+for city, temp in zip(list_city,temperatures):
+    #print(f'The temperature in {city} is {temp} C')
+    
+    if temp< 15:
+        print(f"The temperature in {city} is {temp}°C - ***warm clothes***")
+    else:
+        print(f"The temperature in {city} is {temp}°C")
